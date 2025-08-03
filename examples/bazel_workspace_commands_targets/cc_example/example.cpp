280eb96d100d5cfc37dde2a156206f72aa926616
Lucas Munaretto <lucasmunaretto@gmail.com>
2025-08-03 19:52:06 +0200

    Add `bazel_workspace_commands_targets` workspace
    This workspace supports https://unwrap.hashnode.dev/bazel-basics-part-2

---
+++ examples/bazel_workspace_commands_targets/cc_example/example.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+int main() {
+    printMessage("Unwrap Your Build");
+    return 0;
+}
---
+++ examples/bazel_workspace_commands_targets/py_example/test.py
@@ -0,0 +1,7 @@
+from py_example import example
+
+def test():
+    assert example.main() == None
+
+if __name__ == "__main__":
+    test()
